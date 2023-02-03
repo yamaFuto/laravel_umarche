46382d93591727022f37607817a7ea579e346767
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2023-02-03 10:39:39 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+    {{-- 変数などを分離させたいときに使う --}}
+</div>
