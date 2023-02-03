6c02c2813ee76372d3253979e4857c591a12c2ef
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2023-02-03 09:53:47 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,5 @@
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネントテスト１
 
-    <x-tests.card title="タイトル" content="コンテンツ" />
+    <x-tests.card title="タイトル" content="コンテンツ" :message="$message" />
 </x-tests.app>
