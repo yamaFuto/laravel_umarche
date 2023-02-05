7339391fb3ac018fdb35183fa777db8db92bb678
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2023-02-05 09:30:27 +0900

    アプリ名、ロゴ

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -47,7 +47,7 @@ class LoginRequest extends FormRequest
 
         if($this->routeIs('owner.*')) {
             $guard = 'owners';
-        } elseif($this->routeIs('adin.*')) {
+        } elseif($this->routeIs('admin.*')) {
             $guard = 'admin';
         } else {
             $guard = 'users';
