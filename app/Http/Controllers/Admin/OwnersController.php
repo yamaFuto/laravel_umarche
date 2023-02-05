8389c58a2172997aa64ff2118c67d4b9ed9cae4f
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2023-02-05 21:50:01 +0900

    Store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Owner; // Eloquent エロクアント
 use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダ
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class OwnersController extends Controller
 {
@@ -61,7 +62,21 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', 'min:8', 'string'],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -14,18 +14,21 @@
                           <div class="flex flex-col text-center w-full mb-12">
                             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">オーナー登録</h1>
                           </div>
+                          <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                          <form method="post" action="{{ route('admin.owners.store')}}">
+                            @csrf
                           <div class="lg:w-1/2 md:w-2/3 mx-auto">
                             <div class="-m-2">
                               <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                   <label for="name" class="leading-7 text-sm text-gray-600">オーナー名</label>
-                                  <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required >
+                                  <input type="text" id="name" name="name" value="{{ old('name') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required >
                                 </div>
                               </div>
                               <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                   <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                  <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required >
+                                  <input type="email" id="email" name="email" value="{{ old('email') }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required >
                                 </div>
                               </div>
                               <div class="p-2 w-1/2 mx-auto">
@@ -41,11 +44,12 @@
                                 </div>
                               </div>
                               <div class="p-2 w-full flex justify-around mt-4">
-                                <button onclick="location.href='{{ route('admin.owners.index') }}'" class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
-                                <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                <button type="button" onclick="location.href='{{ route('admin.owners.index') }}'" class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg">戻る</button>
+                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
                               </div>
                               </div>
                           </div>
+                        </form>
                         </div>
                       </section>
                 </div>
