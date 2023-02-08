71f2039c18acf1c78536316e5ad70bd41b4390ac
yamaFuto <fuhiroa28155.7575mt@gmail.com>
2023-02-08 14:46:20 +0900

    image初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +37,9 @@ Route::prefix('shops')
     Route::post('update/{shop}', [ShopController::class, 'update'])
         ->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+    ->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
